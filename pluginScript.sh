2221e5cd569eebc54062b6dd5a21da64e8678e5d
SamuelStern <sterns697@gmail.com>
2018-05-18 19:54:37 +0000

    Renaming and re-commenting several files for clarity

--- pluginScript.sh
+++ pluginScript.sh
@@ -7,7 +7,7 @@ cd svalinn_plugin
 git clone https://github.com/svalinn/DAGMC-Trelis.git
 mkdir bld
 cd bld
-#Up through here works in automatic bash script
+#The following line fails if the script is run not as source
 cmake ../DAGMC-Trelis -DCMAKE_PREFIX_PATH=/opt/Trelis-16.3/bin -DBUILD_IGEOM=ON -DBUILD_IGEOM_TESTS=ON -DBUILD_MCNP_IMPORTER=OFF -DMOAB_DIR=/root/MOAB/install/lib -DDAGMC_DIR=/root/dagmc/DAGMC/
 #Up through here works typed in manually
 #make #This command results in a bug claiming that the system can’t find #“make_watertight/MakeWatertight.hpp”. Maybe I need to set some sharing setting on with #DAGMC, like with MOAB. 
