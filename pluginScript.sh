e240ebd99e6919af3fb8de8dc27c1e19ed82ee11
SamuelStern <sterns697@gmail.com>
2018-05-18 21:12:34 +0000

    Fixed error preventing make from working on plugin

--- pluginScript.sh
+++ pluginScript.sh
@@ -8,7 +8,15 @@ git clone https://github.com/svalinn/DAGMC-Trelis.git
 mkdir bld
 cd bld
 #The following line fails if the script is run not as source
-cmake ../DAGMC-Trelis -DCMAKE_PREFIX_PATH=/opt/Trelis-16.3/bin -DBUILD_IGEOM=ON -DBUILD_IGEOM_TESTS=ON -DBUILD_MCNP_IMPORTER=OFF -DMOAB_DIR=/root/MOAB/install/lib -DDAGMC_DIR=/root/dagmc/DAGMC/
-#Up through here works typed in manually
-#make #This command results in a bug claiming that the system can’t find #“make_watertight/MakeWatertight.hpp”. Maybe I need to set some sharing setting on with #DAGMC, like with MOAB. 
-
+cmake ../DAGMC-Trelis -DCMAKE_PREFIX_PATH=/opt/Trelis-16.3/bin -DBUILD_IGEOM=ON -DBUILD_IGEOM_TESTS=ON -DBUILD_MCNP_IMPORTER=OFF -DMOAB_DIR=/root/MOAB/install/lib -DDAGMC_DIR=/root/dagmc/install/
+make
+PLUGINDIR=/opt/Trelis-16.3/bin/plugins/svalinn
+mkdir $PLUGINDIR
+cp libsvalinn_plugin.so $PLUGINDIR
+cp libiGeom.so $PLUGINDIR
+cp /root/MOAB/install/lib/libMOAB.so.0 $PLUGINDIR
+cp /root/dagmc/install/lib/libmakeWatertight.so $PLUGINDIR
+cd ../DAGMC-Trelis
+cp install.sh $PLUGINDIR
+cd $PLUGINDIR/../..
+bash plugins/svalinn/install.sh
