9c4975c24eae4340e0b8f84b541a860a37979910
Samuel Stern <sterns697@gmail.com>
2018-06-18 20:59:07 +0000

    Added newlines to some obnoxiously long commands

--- trelis_setup.sh
+++ trelis_setup.sh
@@ -5,7 +5,9 @@ INSTALL_ROUTE=$HOME
 cd $INSTALL_ROUTE
 mkdir trelis
 cd trelis
-curl -1 -v --disable-epsv --ftp-skip-pasv-ip -u sstern3@wisc.edu --ftp-ssl \ --output trelis.deb    ftp://ftp.box.com/CNERG/INTERNAL/Resources/Trelis/Trelis-16.3.3-Lin64.deb > trelis.deb;
+curl -1 -v --disable-epsv --ftp-skip-pasv-ip -u sstern3@wisc.edu --ftp-ssl \
+ --output trelis.deb \
+ ftp://ftp.box.com/CNERG/INTERNAL/Resources/Trelis/Trelis-16.3.3-Lin64.deb > trelis.deb;
     #Replace sstern3 with username as needed; external password must be set on box
     #Currently uses Trelis 16.3, since SDK appears incompatible with 16.4
 dpkg -i trelis.deb #This installs Trelis 16.3, files can be found in /opt
