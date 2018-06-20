85e49733b44ff3a7bcf5aca8a73f251a5b819a28
Samuel Stern <sterns697@gmail.com>
2018-06-20 20:20:43 +0000

    Fixed typo breaking sdk download

--- sdk_setup.sh
+++ sdk_setup.sh
@@ -4,7 +4,9 @@
 cd /opt/Trelis-16.3
 mkdir sdk
 cd sdk
-curl -1 -v --disable-epsv --ftp-skip-pasv-ip -u sstern3@wisc.edu --ftp-ssl  --output trelis.deb ftp://ftp.box.com/CNERG/INTERNAL/Resources/Trelis/SDK/Trelis-SDK-16.0.0-Lin64.tar.gz>trelisSDK.gz
+curl -1 -v --disable-epsv --ftp-skip-pasv-ip -u sstern3@wisc.edu\
+ --ftp-ssl  --output trelisSDK.gz\
+ ftp://ftp.box.com/CNERG/INTERNAL/Resources/Trelis/SDK/Trelis-SDK-16.0.0-Lin64.tar.gz >trelisSDK.gz
 tar -xzf trelisSDK.gz 
 cd ../bin
 sudo ln -s ../sdk/bin/* .
