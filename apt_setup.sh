861d6d9cba868d6685b9210099078215330d834d
Samuel Stern <sterns697@gmail.com>
2018-06-26 19:36:56 +0000

    Add gl libraries for trelis

--- apt_setup.sh
+++ apt_setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #Install all simplest-level dependencies of DAGMC-Trelis that can be obtained via apt
-apt install -y libeigen3-dev libtool make mpich libhdf5-dev cmake
+apt install -y libeigen3-dev libtool make mpich libhdf5-dev cmake libgl1-mesa-glx
