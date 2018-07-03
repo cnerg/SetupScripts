11cde00cf3ab7157aacdfccc68f87568fe19cdb4
Samuel Stern <sstern3@wisc.edu>
2018-07-03 18:35:16 -0500

    Fixed silly file addition error

--- MOAB/bld/test/obb/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/samuel/cnerg/SetupScripts/MOAB/moab/test/obb
-# Build directory: /home/samuel/cnerg/SetupScripts/MOAB/bld/test/obb
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(obb_test "/home/samuel/cnerg/SetupScripts/MOAB/bld/bin/obb_test")
