ded79b9ac3c93c22e5e365966c55b47165708b28
Samuel Stern <sstern3@wisc.edu>
2018-07-04 17:03:33 -0500

    Fixed typos

--- plugin_setup.sh
+++ plugin_setup.sh
@@ -2,7 +2,7 @@
  
 #Finally, install the actual svalinn plugin
 INSTALL_ROOT=$HOME/cnerg
-cd $INSTALL_ROUTE
+cd $INSTALL_ROOT
 mkdir svalinn_plugin
 cd svalinn_plugin
 git clone https://github.com/svalinn/DAGMC-Trelis.git
@@ -11,14 +11,14 @@ cd bld
 #The following line fails if the script is run not as source
 cmake ../DAGMC-Trelis -DCMAKE_PREFIX_PATH=/opt/Trelis-16.3/bin\
  -DBUILD_IGEOM=ON -DBUILD_IGEOM_TESTS=ON -DBUILD_MCNP_IMPORTER=OFF\
- -DMOAB_DIR=$INSTALL_ROUTE/MOAB/install/lib -DDAGMC_DIR=$INSTALL_ROUTE/dagmc/install/
+ -DMOAB_DIR=$INSTALL_ROOT/MOAB/install/lib/cmake/MOAB -DDAGMC_DIR=$INSTALL_ROOT/dagmc/install/\
 make
 PLUGINDIR=/opt/Trelis-16.3/bin/plugins/svalinn
 mkdir $PLUGINDIR
 cp libsvalinn_plugin.so $PLUGINDIR
 cp libiGeom.so $PLUGINDIR
-cp $INSTALL_ROUTE/MOAB/install/lib/libMOAB.so.5 $PLUGINDIR
-cp $INSTALL_ROUTE/dagmc/install/lib/libmakeWatertight.so $PLUGINDIR
+cp $INSTALL_ROOT/MOAB/install/lib/libMOAB.so.5 $PLUGINDIR
+cp $INSTALL_ROOT/dagmc/install/lib/libmakeWatertight.so $PLUGINDIR
 cd ../DAGMC-Trelis
 cp install.sh $PLUGINDIR
 cd $PLUGINDIR/../..
